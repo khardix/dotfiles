301f666f8a6f60112748e5cb81bb2a54c2bfce40
Jan Staněk <jstanek@redhat.com>
2017-03-22 13:28:59 +0100

    Add PDF preview in ranger

--- ranger/scope.sh
+++ ranger/scope.sh
@@ -55,9 +55,9 @@ case "$extension" in
     rar)
         try unrar -p- lt "$path" && { dump | trim; exit 0; } || exit 1;;
     # PDF documents:
-    pdf)
-        try pdftotext -l 10 -nopgbrk -q "$path" - && \
-            { dump | trim | fmt -s -w $width; exit 0; } || exit 1;;
+    # pdf)
+    #     try pdftotext -l 10 -nopgbrk -q "$path" - && \
+    #         { dump | trim | fmt -s -w $width; exit 0; } || exit 1;;
     # BitTorrent Files
     torrent)
         try transmission-show "$path" && { dump | trim; exit 5; } || exit 1;;
@@ -84,6 +84,8 @@ case "$mimetype" in
         exiftool "$path" && exit 5
         # Use sed to remove spaces so the output fits into the narrow window
         try mediainfo "$path" && { dump | trim | sed 's/  \+:/: /;';  exit 5; } || exit 1;;
+    application/pdf)
+        pdftoppm -jpeg -singlefile "$path" "${cached//.jpg}" && exit 6;;
 esac
 
 exit 1
