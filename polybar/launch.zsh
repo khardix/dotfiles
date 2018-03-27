3de005ecdfb92b1eaece0b4036b84e05aeeffbf2
Jan Khardix Staněk <khardix@gmail.com>
2018-03-27 22:16:44 +0200

    Make polybar settings more portable

--- polybar/launch.zsh
+++ polybar/launch.zsh
@@ -6,6 +6,9 @@ readonly primary_monitor="$(xrandr -q|grep -w 'primary'|awk '{print $1}')"
 readonly monitor_count=$(xrandr -q|grep -w 'connected'|wc -l)
 
 export PRIMARY_MONITOR=${primary_monitor:-eDP-1-1}
+export ETH="$(basename /sys/class/net/en*(onY1))"  # first ethernet device
+export WLN="$(basename /sys/class/net/wl*(onY1))"  # first wifi device
+
 if [[ "$monitor_count" -gt 1 ]]; then
     polybar center &!
     polybar left &!
